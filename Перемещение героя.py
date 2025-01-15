d5eb3061567855446503362d3462dba210928c63
Timgame <timofei50200@gmail.com>
2025-01-15 18:33:15 +0300

    Перемещение героя

---
+++ Перемещение героя.py
@@ -0,0 +1,137 @@
+import pygame
+import sys
+import os
+
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+def load_level(filename):
+    filename = "data/" + filename
+    with open(filename, "r") as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    max_width = max(map(len, level_map))
+    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '.':
+                Tile('empty', x, y)
+            elif level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                Tile('empty', x, y)
+                new_player = Player(x, y)
+    return new_player, x, y
+
+
+def start_screen():
+    intro_text = ["Перемещение героя", "",
+                  "Герой двигается",
+                  "Карта на месте"]
+    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 50
+
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('black'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or event.type == pygame.MOUSEBUTTONDOWN:
+                game()
+            pygame.display.flip()
+            clock.tick(FPS)
+
+
+def game():
+    level = load_level('map.txt')
+    player, level_x, level_y = generate_level(level)
+    player.mask = pygame.mask.from_surface(player.image)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                x, y = player.pos
+                if keys[pygame.K_UP]:
+                    if y > 0 and (level[y - 1][x] == "." or level[y - 1][x] == "@"):
+                        player.move(x, y - 1)
+                elif keys[pygame.K_DOWN]:
+                    if y < level_y - 1 and (level[y + 1][x] == "." or level[y + 1][x] == "@"):
+                        player.move(x, y + 1)
+                elif keys[pygame.K_RIGHT]:
+                    if x < level_x - 1 and (level[y][x + 1] == "." or level[y][x + 1] == "@"):
+                        player.move(x + 1, y)
+                elif keys[pygame.K_LEFT]:
+                    if x > 0 and (level[y][x - 1] == "." or level[y][x - 1] == "@"):
+                        player.move(x - 1, y)
+        tiles_group.draw(screen)
+        player_group.draw(screen)
+        pygame.display.flip()
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.pos = (pos_x, pos_y)
+
+    def move(self, x, y):
+        self.pos = (x, y)
+        self.rect = self.image.get_rect().move(tile_width * self.pos[0] + 15, tile_height * self.pos[1] + 5)
+
+
+player = None
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+tile_images = {
+    'wall': load_image('box.png'),
+    'empty': load_image('grass.png')
+}
+player_image = load_image('mar.png')
+tile_width = tile_height = 50
+
+FPS = 50
+pygame.init()
+size = width, height = 500, 500
+screen = pygame.display.set_mode(size)
+clock = pygame.time.Clock()
+
+start_screen()
